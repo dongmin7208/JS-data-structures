cc64606f0ec95438fa96303a98e9ab7cdc880010
parkdongmin <park@arcphili.co.jp>
2022-04-30 15:03:05 +0900

    CountUniqueValues_Q, S

---
+++ src/multiplePointers/CountUniqueValues_Q.jsx
@@ -0,0 +1,16 @@
+/*
+Q.
+Implement a function called countUniqueValues, which accepts a sorted array,and counts the unique values in the array. There can be nagative numbers in the array, but it will always be sorted.
+
+countUniqueValuesという関数を実装します。この関数は、ソートされた配列を受け入れ、配列内の一意の値をカウントします。 配列にはネイティブ番号を指定できますが、常にソートされます。
+
+count Unique Values라는 함수를 구현합니다.이 함수는 정렬된 배열을 받아들여 배열 내의 하나의 값을 카운트합니다. 배열에는 원어민 번호를 지정할 수 있지만 항상 정렬됩니다.
+
+*/
+function countUniqueValues(){
+    
+}
+countUniqueValues([1,1,1,1,1,2]) //2 
+countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) //7 
+countUniqueValues([]) // 0 
+countUniqueValues([-2,-1,-1,0,1]) //4 
