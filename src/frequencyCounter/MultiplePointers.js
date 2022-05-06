cbaca25fe93a2366b9c9c78f5224e1a6d1c938b4
parkdongmin <park@arcphili.co.jp>
2022-05-06 09:38:12 +0900

    MulitiplePointers

---
+++ src/frequencyCounter/MultiplePointers.js
@@ -0,0 +1,20 @@
+/*
+
+    Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in. You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+
+
+    areaThereDuplicatesという関数を実装します。areaThereDuplicatesは変数数の引数を受け入れ、渡された引数に重複がないかどうかを確認します。 これは、周波数カウンタパターンまたは複数のポインタパターンを使用して解決できます。
+
+    areaThere Duplicates라고 하는 함수를 구현합니다.areaThere Duplicates는 변수 수의 인수를 받아들이고 전달된 인수에 중복이 없는지 확인합니다. 이것은, 주파수 카운터 패턴 또는 복수의 포인터 패턴을 사용해 해결할 수 있습니다.
+
+    Examples:
+        areThereDuplicates(1,2,3)// false
+        areThereDuplicates(1,2,2)// ture
+
+*/
+    function areThereDuplicates(){
+
+    }
+
+    areThereDuplicates(1,2,3)// false
+    areThereDuplicates(1,2,2)// ture
